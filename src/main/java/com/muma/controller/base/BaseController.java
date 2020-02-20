e54aa37b2f5a381436a5f2750f5725708e63b8b4
Your Name <you@example.com>
2020-02-20 22:48:39 +0800

    1.修改框架结构

---
+++ src/main/java/com/muma/controller/base/BaseController.java
@@ -0,0 +1,52 @@
+package com.muma.controller.base;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.muma.common.HttpContext.getResponse;
+
+
+/**
+ * @Date 17:47 2019/4/15
+ * @Remark  controller基类
+ **/
+public class BaseController {
+	private static final Logger log = Logger.getLogger(BaseController.class);
+	
+	protected void printJSON(HttpServletResponse response,JSONObject json){
+		PrintWriter out = null;
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		try {
+			out = response.getWriter();
+			out.print(JSON.toJSONString(json, SerializerFeature.DisableCircularReferenceDetect));
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}finally{
+			out.close();
+		}
+		
+	}
+
+	public String jsonSuccess(JSONObject json){
+		json.put("success", true);
+		printJSON(getResponse(),json);
+		return null;
+	}
+
+	public String jsonFail(String msg){
+		JSONObject obj = new JSONObject(2);
+		obj.put("success", false);
+		obj.put("note", msg);
+		printJSON(getResponse(), obj);
+		return null;
+	}
+	
+
+}
