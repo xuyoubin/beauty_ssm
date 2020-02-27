5f5358915a7da22f7d2370c8e9b755b7e7fbc170
Your Name <you@example.com>
2020-02-27 23:54:55 +0800

    添加分页查询

--- src/main/java/com/muma/service/base/BaseService.java
+++ src/main/java/com/muma/service/base/BaseService.java
@@ -1,5 +1,7 @@
 package com.muma.service.base;
 
+import com.muma.common.Page;
+import com.muma.common.PageRequest;
 import com.muma.common.Sort;
 
 import java.util.List;
@@ -10,5 +12,23 @@ public interface BaseService<T> {
     List<T> findAll();
     List<T> findList(Map<String, Object> var1);
     List<T> findList(Map<String, Object> var1, Sort var2);
+
+    Page<T> findPage(PageRequest var1, Map<String, Object> var2);
+
+    int count(Map<String, Object> var1);
+
+    int create(T var1);
+
+    int createByFull(T var1);
+
+    int update(T var1);
+
+    int updateByFull(T var1);
+
+    int deleteByPrimaryKey(Object var1);
+
+    int delete(T var1);
+
+    int updateByPrimaryKey(T var1);
 
 }
--- src/main/java/com/muma/service/base/impl/BaseServiceImpl.java
+++ src/main/java/com/muma/service/base/impl/BaseServiceImpl.java
@@ -11,9 +11,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import com.muma.common.Sort;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.muma.common.*;
 import com.muma.service.base.BaseService;
-import com.sun.jndi.toolkit.dir.SearchFilter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import tk.mybatis.mapper.common.Mapper;
@@ -33,14 +34,17 @@ public class BaseServiceImpl<T> implements BaseService<T> {
     public T getEntity(Object key) {
         return this.mapper.selectByPrimaryKey(key);
     }
+
     @Override
     public List<T> findAll() {
         return this.mapper.selectAll();
     }
+
     @Override
     public List<T> findList(Map<String, Object> searchParams) {
         return this.findList(searchParams, (Sort)null);
     }
+
     @Override
     public List<T> findList(Map<String, Object> searchParams, Sort sort) {
         Example example = new Example(this.clazz);
@@ -55,6 +59,7 @@ public class BaseServiceImpl<T> implements BaseService<T> {
         return list;
     }
 
+    @Override
     public Page<T> findPage(PageRequest pageRequest, Map<String, Object> searchParams) {
         Example example = new Example(this.clazz);
         Criteria criteria = example.createCriteria();
@@ -69,14 +74,7 @@ public class BaseServiceImpl<T> implements BaseService<T> {
         return page;
     }
 
-    public int countAll() {
-        return this.mapper.selectCount((Object)null);
-    }
-
-    public int count(T t) {
-        return this.mapper.selectCount(t);
-    }
-
+    @Override
     public int count(Map<String, Object> searchParams) {
         Example example = new Example(this.clazz);
         Criteria criteria = example.createCriteria();
@@ -86,81 +84,39 @@ public class BaseServiceImpl<T> implements BaseService<T> {
         return count;
     }
 
+    @Override
     public int create(T entity) {
         return this.mapper.insertSelective(entity);
     }
 
+    @Override
     public int createByFull(T entity) {
         return this.mapper.insert(entity);
     }
 
+    @Override
     public int update(T entity) {
         return this.mapper.updateByPrimaryKeySelective(entity);
     }
 
+    @Override
     public int updateByFull(T entity) {
         return this.mapper.updateByPrimaryKey(entity);
     }
 
+    @Override
     public int deleteByPrimaryKey(Object key) {
         return this.mapper.deleteByPrimaryKey(key);
     }
 
+    @Override
     public int delete(T entity) {
         return entity == null ? 0 : this.mapper.delete(entity);
     }
 
+    @Override
     public int updateByPrimaryKey(T entity) {
         return this.mapper.updateByPrimaryKeySelective(entity);
     }
 
-    public int save(T entity) {
-        return this.mapper.insertSelective(entity);
-    }
-
-    public PageInfo<T> selectPaged(com.hk.base.dto.request.PageRequest page, Map<String, Object> params) {
-        Example example = new Example(this.clazz);
-        Criteria criteria = example.createCriteria();
-        Map<String, SearchFilter> filters = SearchFilter.parse(params);
-        DynamicCriteria.bySearchFilter(filters, criteria);
-        PageHelper.startPage(page.getPage().intValue(), page.getRows().intValue());
-        if (StringUtils.isNotEmpty(page.getSort())) {
-            page.setSort(StringUtil.camelhumpToUnderline(page.getSort()));
-        }
-
-        if (StringUtils.isNotEmpty(page.getSort()) && StringUtils.isNotEmpty(page.getOrder())) {
-            String sort = page.getSort() + " " + page.getOrder();
-            PageHelper.orderBy(sort);
-        }
-
-        List<T> list = this.mapper.selectByExample(example);
-        PageInfo<T> pageInfo = new PageInfo(list);
-        return pageInfo;
-    }
-
-    public List<T> query() {
-        return this.mapper.selectAll();
-    }
-
-    public List<T> query(Map<String, Object> params) {
-        Example example = new Example(this.clazz);
-        Criteria criteria = example.createCriteria();
-        Map<String, SearchFilter> filters = SearchFilter.parse(params);
-        DynamicCriteria.bySearchFilter(filters, criteria);
-        List<T> list = this.mapper.selectByExample(example);
-        return list;
-    }
-
-    public List<T> query(Map<String, Object> params, String orderByClause) {
-        Example example = new Example(this.clazz);
-        if (StringUtils.isNotBlank(orderByClause)) {
-            example.setOrderByClause(orderByClause);
-        }
-
-        Criteria criteria = example.createCriteria();
-        Map<String, SearchFilter> filters = SearchFilter.parse(params);
-        DynamicCriteria.bySearchFilter(filters, criteria);
-        List<T> list = this.mapper.selectByExample(example);
-        return list;
-    }
 }
