5843ccc605503586250eacb9274c6371cb560e73
xyb <513592330@qq.com>
2020-03-13 16:23:01 +0800

    添加 dao.xml

--- src/main/java/com/muma/dao/TaskOrderDao.java
+++ src/main/java/com/muma/dao/TaskOrderDao.java
@@ -1,6 +1,12 @@
 package com.muma.dao;
 
 import com.muma.entity.TaskOrder;
+import com.muma.enums.OperateStatusEnum;
+import com.muma.enums.StatusEnum;
+import com.muma.enums.TaskTypeEnum;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 销售浏览发布任务
@@ -11,5 +17,12 @@ public interface TaskOrderDao {
      * @param
      */
     void addTaskOrder(TaskOrder taskOrder);
+    /**
+     * 根据条件查询任务列表
+     * @return
+     */
+    List<TaskOrder> queryTaskOrderList(@Param("regPhone") String regPhone, @Param("shopId") Integer shopId, @Param("type") TaskTypeEnum type,
+                                       @Param("operateStatus") OperateStatusEnum operateStatus, @Param("status") StatusEnum status,
+                                       @Param("pageIndex") Integer pageIndex, @Param("pageSize") Integer pageSize);
     
 }
